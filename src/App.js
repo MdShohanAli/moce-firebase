2e2abdd45ff55e3d449bcc072924e6b17466adc5
MdShohanAli <720shohan@gmail.com>
2022-12-14 14:06:56 +0600

    final push

--- src/App.js
+++ src/App.js
@@ -1,21 +1,15 @@
 
 import './App.css';
-import { getAuth, GoogleAuthProvider, signInWithPopup, signOut } from "firebase/auth";
+import { getAuth, GithubAuthProvider, GoogleAuthProvider, signInWithPopup, signOut } from "firebase/auth";
 import app from './firebase.init';
 import { useState } from 'react';
 const auth = getAuth(app);
 function App() {
   const [user, setUser] = useState({})
   const googleProvider = new GoogleAuthProvider()
-  const handlelogOut = () => {
-    signOut(auth)
-      .then(() => {
-        setUser({})
-      })
-      .cath(() => {
-        setUser({})
-      })
-  }
+  const githubProvider = new GithubAuthProvider()
+
+
   const handlegooleProvider = () => {
     signInWithPopup(auth, googleProvider)
       .then(result => {
@@ -26,15 +20,41 @@ function App() {
       .cath(error => {
         console.error("error", error)
       })
+
+  }
+  const handlegithubProvider = () => {
+    signInWithPopup(auth, githubProvider)
+      .then(result => {
+        const user = result.user
+        setUser(user)
+        console.log(user);
+      })
+      .catch(error => {
+        console.error("error", error)
+      })
+  }
+  const handlelogOut = () => {
+    signOut(auth)
+      .then(() => {
+        setUser({})
+      })
+      .cath(() => {
+        setUser({})
+      })
   }
 
   return (
     <div className="App">
       {
         user.uid ? <button onClick={handlelogOut}> Sign out </button> :
-          <button onClick={handlegooleProvider}> Sign In With Google </button>
+          <>
+            <button onClick={handlegooleProvider}> Sign In With Google </button>
+            <button onClick={handlegithubProvider}> Sign in With github </button>
+          </>
 
       }
+
+
 
       <h2> Name : {user.displayName} </h2>
       <img src={user.photoURL} alt="" />
